f65c29f9cf7a05efad6849f54b013ef9b68d1457
Juanmilla-07 <juanma28tf@gmail.com>
2026-03-26 20:03:20 +0100

    Codigo Refactorizado

--- Calculadora.java
+++ Calculadora.java
@@ -1,12 +1,13 @@
 package Practica;
 
 public class Calculadora {
-
-    public int sumar(int numero1, int numero2) {
-        return numero1 + numero2;
-    }
-
-    public void mostrarResultado(int resultado) {
+    public void mostrarResultado(int numero1, int numero2) {
+        int resultado = sumar(numero1, numero2);
         System.out.println("Resultado: " + resultado);
+    }
+    
+    public int sumar(int numero1, int numero2) {
+        int resultado = numero1 + numero2;
+        return resultado;
     }
 }
