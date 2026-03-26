feab2dabfdf8d9c167e921a983e78263e54ab192
Juanmilla-07 <juanma28tf@gmail.com>
2026-03-26 14:27:47 +0100

    Refactorizacion: Separacion de suma y salida del programa

--- Calculadora.java
+++ Calculadora.java
@@ -1,8 +1,12 @@
 package Practica;
 
-public class Calculadora{
-    public void sumar(int numero1, int numero2) {
-        int resultado = numero1 + numero2;
+public class Calculadora {
+
+    public int sumar(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+
+    public void mostrarResultado(int resultado) {
         System.out.println("Resultado: " + resultado);
     }
 }
