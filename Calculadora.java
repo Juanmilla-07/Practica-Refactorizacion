a5c7d6d73ef472ab23c0ad9265855e132fda9d88
Juanmilla-07 <juanma28tf@gmail.com>
2026-03-26 14:17:06 +0100

    Refactorización: Mejora de Variables

---
+++ Calculadora.java
@@ -0,0 +1,8 @@
+package Practica;
+
+public class Calculadora{
+    public void sumar(int numero1, int numero2) {
+        int resultado = numero1 + numero2;
+        System.out.println("Resultado: " + resultado);
+    }
+}
